d24e364372af2100e0ed8c204a6007be53a3df8f
PeeranatKongsang <peeranatkong4099@gmail.com>
2025-08-20 14:33:09 +0700

    third push

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
     // input the point from user of student 
-    $AM
-    $AH
-    $AS
-    
+    $AM = 56
+    $AH = 89
+    $AS = 78
+    $BM = 45
+    $BH = 67
+    $BS = 90
+    $CM = 88
+    $CH = 76    
+    $CS = 65
+    $DM = 55
+    $DH = 45
+    $DS = 78
+
+    echo "The point of student A is: $AM, $AH, $AS <br>";
+    echo "The point of student B is: $BM, $BH, $BS <br>";
+    echo "The point of student C is: $CM, $CH, $CS <br>";   
+    echo "The point of student D is: $DM, $DH, $DS <br>";
+    echo "<br>";
+
 ?>
